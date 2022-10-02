30730251f000b34051399ca0eade0b43d5f4f022
yuliiakaras <yuliiakaras03@gmail.com>
2022-10-02 22:43:54 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const navBgOverlayEl = document.querySelector('.nav__bgOverlay');
+const navList = document.querySelector('.nav__list');
+const navCloseEl = document.querySelector('.nav__close');
+const navIconEl = document.querySelector('.nav__icon');
+
+const navOpen = () => {
+	navList.classList.add('show');
+	navBgOverlayEl.classList.add('active');
+	document.body.style = 'visibility: visible; height: 100vh; width: 100vw; overflow: hidden;'; 
+}
+
+const navClose = () => {
+	navList.classList.remove('show');
+	navBgOverlayEl.classList.remove('active');
+	document.body.style = 'visibility: visible; height: initial; width: 100%; overflow-x: hidden;';
+}
+
+navIconEl.addEventListener('click', navOpen);
+navCloseEl.addEventListener('click', navClose);
+navBgOverlayEl.addEventListener('click', navClose);
+
+// aos
+
+AOS.init({
+	offset: 200,
+	delay: 100,
+	duration: 400,
+	easing: 'easy',
+	once: false,
+	mirror: false,
+	anchorPlacement: 'top-bottom'
+})
+
